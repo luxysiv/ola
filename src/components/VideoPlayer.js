cf086efe020d9f544133fe6a2d634b0539cf5168
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2026-02-08 23:42:34 +0700

    Create VideoPlayer.js

---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,22 @@
+import React, { useRef, useEffect } from "react";
+import Hls from "hls.js";
+
+const VideoPlayer = ({ src }) => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      if (Hls.isSupported()) {
+        const hls = new Hls();
+        hls.loadSource(src);
+        hls.attachMedia(videoRef.current);
+      } else if (videoRef.current.canPlayType("application/vnd.apple.mpegurl")) {
+        videoRef.current.src = src;
+      }
+    }
+  }, [src]);
+
+  return <video ref={videoRef} controls style={{ width: "100%" }} />;
+};
+
+export default VideoPlayer;
