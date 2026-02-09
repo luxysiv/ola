0bdd6c3325774f48b6b39c6141c62462dce1333b
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2026-02-09 20:02:53 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,35 +5,47 @@ import { Link } from "react-router-dom";
 
 function Home() {
   const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
-
     axios
       .get("https://phimapi.com/danh-sach/phim-moi-cap-nhat-v3?page=1")
-      .then(res => {
-        setMovies(res.data.items || []);
-      })
-      .catch(() => setMovies([]))
-      .finally(() => setLoading(false));
+      .then(res => setMovies(res.data.items || []));
   }, []);
 
   return (
     <div>
       <h2>Phim mới cập nhật</h2>
 
-      {loading && <p>Đang tải...</p>}
-
-      <ul>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, 150px)",
+          gap: 15
+        }}
+      >
         {movies.map(m => (
-          <li key={m._id}>
-            <Link to={`/movie/${m.slug}`}>
-              {m.name}
-            </Link>
-          </li>
+          <Link
+            key={m._id}
+            to={`/movie/${m.slug}`}
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <div>
+              <img
+                src={m.poster_url}
+                alt={m.name}
+                width="150"
+                style={{ borderRadius: 6 }}
+              />
+              <div style={{ marginTop: 5 }}>
+                <b>{m.name}</b>
+              </div>
+              <small>
+                {m.year} • {m.quality}
+              </small>
+            </div>
+          </Link>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
