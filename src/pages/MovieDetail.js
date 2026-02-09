30b39905abeebee45c30176d9aafc14d7291b757
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2026-02-09 20:07:04 +0700

    Ok

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,3 +1,4 @@
+// src/pages/MovieDetail.js
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
@@ -5,22 +6,61 @@ import VideoPlayer from "../components/VideoPlayer";
 
 function MovieDetail() {
   const { slug } = useParams();
+
   const [movie, setMovie] = useState(null);
+  const [episodes, setEpisodes] = useState([]);
   const [src, setSrc] = useState(null);
 
   useEffect(() => {
     axios.get(`https://phimapi.com/phim/${slug}`)
       .then(res => {
-        setMovie(res.data);
-        const link = res.data.episodes[0].server_data[0].link_m3u8;
-        setSrc(link);
+        const data = res.data;
+
+        setMovie(data.movie);
+
+        const epList =
+          data.episodes?.[0]?.server_data || [];
+
+        setEpisodes(epList);
+
+        if (epList.length > 0) {
+          setSrc(epList[0].link_m3u8);
+        }
+      })
+      .catch(() => {
+        setMovie(null);
+        setEpisodes([]);
+        setSrc(null);
       });
   }, [slug]);
 
   return (
     <div>
-      {movie && <h2>{movie.movie.name}</h2>}
+      {movie && (
+        <>
+          <h2>{movie.name}</h2>
+          <p>{movie.origin_name}</p>
+        </>
+      )}
+
       {src && <VideoPlayer src={src} />}
+
+      {/* Danh sách tập */}
+      {episodes.length > 1 && (
+        <div style={{ marginTop: 15 }}>
+          <h3>Danh sách tập</h3>
+          <div style={{ display: "flex", flexWrap: "wrap", gap: 8 }}>
+            {episodes.map((ep, index) => (
+              <button
+                key={index}
+                onClick={() => setSrc(ep.link_m3u8)}
+              >
+                {ep.name}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
