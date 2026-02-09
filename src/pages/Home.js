e20ce7a755ebeef8a198b9a08ae80872813bf27a
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2026-02-09 20:00:27 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,36 @@
+// src/pages/Home.js
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 function Home() {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    axios.get("https://phimapi.com/danh-sach/phim-moi-cap-nhat?page=1")
-      .then(res => setMovies(res.data.items));
+    setLoading(true);
+
+    axios
+      .get("https://phimapi.com/danh-sach/phim-moi-cap-nhat-v3?page=1")
+      .then(res => {
+        setMovies(res.data.items || []);
+      })
+      .catch(() => setMovies([]))
+      .finally(() => setLoading(false));
   }, []);
 
   return (
     <div>
       <h2>Phim mới cập nhật</h2>
+
+      {loading && <p>Đang tải...</p>}
+
       <ul>
         {movies.map(m => (
           <li key={m._id}>
-            <Link to={`/movie/${m.slug}`}>{m.name}</Link>
+            <Link to={`/movie/${m.slug}`}>
+              {m.name}
+            </Link>
           </li>
         ))}
       </ul>
