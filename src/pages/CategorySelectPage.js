3e5a322400c6e794b1b69f14e00ae1d792ce4f63
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2026-02-10 23:32:40 +0700

    Create CategorySelectPage.js

---
+++ src/pages/CategorySelectPage.js
@@ -0,0 +1,38 @@
+// src/pages/CategorySelectPage.js
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Container, Typography, Select, MenuItem } from "@mui/material";
+
+function CategorySelectPage() {
+  const [categories, setCategories] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get("https://phimapi.com/the-loai")
+      .then(res => setCategories(res.data || []))
+      .catch(() => setCategories([]));
+  }, []);
+
+  return (
+    <Container sx={{ mt: 3 }}>
+      <Typography variant="h5" gutterBottom>
+        Chọn thể loại
+      </Typography>
+      <Select
+        fullWidth
+        value=""
+        onChange={e => navigate(`/category/${e.target.value}`)}
+      >
+        <MenuItem value="">--Chọn thể loại--</MenuItem>
+        {categories.map(c => (
+          <MenuItem key={c._id} value={c.slug}>
+            {c.name}
+          </MenuItem>
+        ))}
+      </Select>
+    </Container>
+  );
+}
+
+export default CategorySelectPage;
