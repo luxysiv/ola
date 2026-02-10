3aade3367d968d69c0d46bfea65ad232d57456e4
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2026-02-10 23:34:10 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
@@ -6,7 +5,12 @@ import CategoryPage from "./pages/CategoryPage";
 import CountryPage from "./pages/CountryPage";
 import YearPage from "./pages/YearPage";
 import MovieDetail from "./pages/MovieDetail";
-import TypeListPage from "./pages/TypeListPage"; // thêm mới
+import TypeListPage from "./pages/TypeListPage";
+
+import CategorySelectPage from "./pages/CategorySelectPage";
+import CountrySelectPage from "./pages/CountrySelectPage";
+import YearSelectPage from "./pages/YearSelectPage";
+import TypeListSelectPage from "./pages/TypeListSelectPage";
 
 function App() {
   return (
@@ -15,21 +19,28 @@ function App() {
         <Link to="/">Trang chủ</Link> |{" "}
         <Link to="/search">Tìm kiếm</Link> |{" "}
         <Link to="/category">Thể loại</Link> |{" "}
-        <Link to="/country/">Quốc gia</Link> |{" "}
+        <Link to="/country">Quốc gia</Link> |{" "}
         <Link to="/year">Năm</Link> |{" "}
-        <Link to="/list/phim-bo">Phim Bộ</Link> |{" "}
-        <Link to="/list/phim-le">Phim Lẻ</Link> |{" "}
-        <Link to="/list/hoat-hinh">Hoạt Hình</Link>
+        <Link to="/list">Loại phim</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
+
+        {/* Trang lựa chọn */}
+        <Route path="/category" element={<CategorySelectPage />} />
+        <Route path="/country" element={<CountrySelectPage />} />
+        <Route path="/year" element={<YearSelectPage />} />
+        <Route path="/list" element={<TypeListSelectPage />} />
+
+        {/* Trang chi tiết */}
         <Route path="/category/:category" element={<CategoryPage />} />
         <Route path="/country/:country" element={<CountryPage />} />
         <Route path="/year/:year" element={<YearPage />} />
+        <Route path="/list/:type_list" element={<TypeListPage />} />
+
         <Route path="/movie/:slug" element={<MovieDetail />} />
-        <Route path="/list/:type_list" element={<TypeListPage />} /> {/* mới */}
       </Routes>
     </Router>
   );
