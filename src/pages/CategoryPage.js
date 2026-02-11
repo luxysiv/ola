c12501c08301c3667a6818ea46719d5038f9de51
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2026-02-11 11:55:22 +0700

    Update CategoryPage.js

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -57,7 +57,7 @@ function CategoryPage() {
         fullWidth
         value={category || ""}
         onChange={e =>
-          navigate(`/category/${e.target.value}`)
+          navigate(`/the-loai/${e.target.value}`)
         }
       >
         <MenuItem value="">--Chọn thể loại--</MenuItem>
@@ -81,7 +81,7 @@ function CategoryPage() {
         {movies.map(m => (
           <Grid item xs={6} sm={4} md={3} lg={2} key={m._id}>
             <Card>
-              <Link to={`/movie/${m.slug}`}>
+              <Link to={`/phim/${m.slug}`}>
                 <CardMedia
                   component="img"
                   height="250"
