363b0ae5b9147e8464b5864a04ce8544cd458bd4
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2026-02-13 08:47:18 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-// src/App.js
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import CategoryPage from "./pages/CategoryPage";
@@ -9,21 +10,37 @@ import MovieDetail from "./pages/MovieDetail";
 import TypeListPage from "./pages/TypeListPage";
 import Header from "./components/Header";
 import LatestPage from "./pages/LatestPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/phim-moi-cap-nhat" element={<LatestPage />} />
-        <Route path="/tim-kiem" element={<Search />} />
-        <Route path="/the-loai/:category" element={<CategoryPage />} />
-        <Route path="/quoc-gia/:country" element={<CountryPage />} />
-        <Route path="/nam/:year" element={<YearPage />} />
-        <Route path="/danh-sach/:type_list" element={<TypeListPage />} />
-        <Route path="/phim/:slug" element={<MovieDetail />} />
-      </Routes>
+      {/* layout full height */}
+      <Box
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column"
+        }}
+      >
+        <Header />
+
+        {/* nội dung */}
+        <Box sx={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/phim-moi-cap-nhat" element={<LatestPage />} />
+            <Route path="/tim-kiem" element={<Search />} />
+            <Route path="/the-loai/:category" element={<CategoryPage />} />
+            <Route path="/quoc-gia/:country" element={<CountryPage />} />
+            <Route path="/nam/:year" element={<YearPage />} />
+            <Route path="/danh-sach/:type_list" element={<TypeListPage />} />
+            <Route path="/phim/:slug" element={<MovieDetail />} />
+          </Routes>
+        </Box>
+
+        <Footer />
+      </Box>
     </Router>
   );
 }
